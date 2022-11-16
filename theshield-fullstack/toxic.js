53946b24074c0d38a9309a747f34cf4cdfd9c4c0
toxicking-techguru <73772907+toxicking-techguru@users.noreply.github.com>
2022-11-16 11:00:09 +0300

    removed unnecessary visual studio files
    made somechanges in html file

--- theshield-fullstack/toxic.js
+++ theshield-fullstack/toxic.js
@@ -1,6 +1,6 @@
 
  const timeH = document.querySelector ('P');
- let timeSecond = 120;
+ let timeSecond = 50;
  timeH.innerHTML = `you can wait for${timeSecond}`;
 
  const countDown = setInterval (()=>{
